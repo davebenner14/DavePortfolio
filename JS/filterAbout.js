167c8111f7200a5d8c3516b80a83f1dcdfd009ab
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-10-26 13:13:26 -0400

    Debug styling issues and jquery issues with skills and about me sections

--- JS/filterAbout.js
+++ JS/filterAbout.js
@@ -1,17 +1,9 @@
 $(document).ready(function () {
-  $(".filter-button").click(function () {
+  $(".about-filter-button").click(function () {
     const filterType = $(this).attr("data-filter");
-
-    // remove active class from all buttons
-    $(".filter-button").removeClass("active");
-
-    // add active class to clicked button
+    $(".about-filter-button").removeClass("active");
     $(this).addClass("active");
-
-    // hide all about content
     $(".about-content").addClass("hidden");
-
-    // show the selected version
     $(`#${filterType}-version`).removeClass("hidden");
   });
 });
