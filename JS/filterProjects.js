aa20d32f702b2444d8ea9f74d537d755706e29b1
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-10-26 14:16:48 -0400

    Add project filtering buttons with styling

---
+++ JS/filterProjects.js
@@ -0,0 +1,9 @@
+function showGenres() {
+  document.getElementById("genres").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguages() {
+  document.getElementById("languages").style.display = "block";
+  document.getElementById("genres").style.display = "none";
+}
